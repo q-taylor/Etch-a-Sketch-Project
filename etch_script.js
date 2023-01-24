2852bb873078e3c0441c0a2087d54805d35a6cb5
quinn <q.taylor@gmail.com>
2023-01-24 16:41:39 -0500

    made each grid-box display as an equal size

--- etch_script.js
+++ etch_script.js
@@ -1,22 +1,25 @@
 // get grid-container element and store in constant for use
 const gridContainer = document.getElementById('grid-container');
 
-let row = 16;
-let column = 16;
+let gridSize = 16;
+
+
+// set height and width of grid box depending on grid size
+//
 // loop that creates div grid
 //  create  div for each row up to the number of columns
 //  in each row div create square divs up to the number per row
 
-for (let i = 0; i < row; i++) {   //create row divs
+for (let i = 0; i < gridSize; i++) {   //create row divs
     const row = document.createElement('div');
     row.classList.add (`row${i+1}`, 'row');
-    row.style.height = 960/row;
 
     gridContainer.appendChild (row);
-    for (let j = 0; j < column; j++) {      // create each grid-box in the row
+    for (let j = 0; j < gridSize; j++) {      // create each grid-box in the row
         const column = document.createElement('div');
         column.classList.add (`column${j+1}`, 'grid-box');
-
         row.appendChild (column);
+
+
     }
 }
