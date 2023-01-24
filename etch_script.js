bf057abaf80737980e49f8b50894ed6ccb214bc9
quinn <q.taylor@gmail.com>
2023-01-24 18:24:49 -0500

    added check and reprompt for gridSize > 100 reduced grid-box border thickness

--- etch_script.js
+++ etch_script.js
@@ -11,17 +11,22 @@ drawGrid(gridSize);
 // function that creates div grid
 
 function drawGrid (gridSize) {
-    for (let i = 1; i <= gridSize; i++) {   //create row divs
-        const row = document.createElement('div');
-        row.classList.add (`row${i}`, 'row');
+    if (gridSize <= 100) {
+        for (let i = 1; i <= gridSize; i++) {   //create row divs
+            const row = document.createElement('div');
+            row.classList.add (`row${i}`, 'row');
 
-        gridContainer.appendChild (row);
-        for (let j = 1; j <= gridSize; j++) {      // create each grid-box in the row
-            const column = document.createElement('div');
-            column.classList.add (`column${j}`, 'grid-box');
-            column.id = `${i}-${j}`;
-            row.appendChild (column);
+            gridContainer.appendChild (row);
+            for (let j = 1; j <= gridSize; j++) {      // create each grid-box in the row
+                const column = document.createElement('div');
+                column.classList.add (`column${j}`, 'grid-box');
+                column.id = `${i}-${j}`;
+                row.appendChild (column);
+            }
         }
+    } else {
+        gridSize = prompt('That was too big, 100 or under please. What size grid would you like?');
+        drawGrid(gridSize);
     }
 }
 
@@ -29,4 +34,5 @@ function drawGrid (gridSize) {
 gridContainer.addEventListener('mouseover', function(e) {
     const box = e.target;
     box.style.backgroundColor = 'black';
+    console.log(box);
 });
