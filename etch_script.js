14965b41cf0d212ec6b60d19da67531cf41dfb87
quinn <q.taylor@gmail.com>
2023-01-24 16:52:00 -0500

    added classes to each grid-box corresponding to their row-column cooridnates

--- etch_script.js
+++ etch_script.js
@@ -10,14 +10,14 @@ let gridSize = 16;
 //  create  div for each row up to the number of columns
 //  in each row div create square divs up to the number per row
 
-for (let i = 0; i < gridSize; i++) {   //create row divs
+for (let i = 1; i <= gridSize; i++) {   //create row divs
     const row = document.createElement('div');
-    row.classList.add (`row${i+1}`, 'row');
+    row.classList.add (`row${i}`, 'row');
 
     gridContainer.appendChild (row);
-    for (let j = 0; j < gridSize; j++) {      // create each grid-box in the row
+    for (let j = 1; j <= gridSize; j++) {      // create each grid-box in the row
         const column = document.createElement('div');
-        column.classList.add (`column${j+1}`, 'grid-box');
+        column.classList.add (`column${j}`, 'grid-box', `${i}-${j}`);
         row.appendChild (column);
 
 
